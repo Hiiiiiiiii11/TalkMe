be4c0c74838aac3eec20e7590e3bf87eccd9afad
Quoc Huy <hhh1z1369@gmail.com>
2025-12-30 00:59:59 +0700

    add Client for Share

---
+++ Helper/GrpcClient/GrpcResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.GrpcClient
+{
+    public class GrpcResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T? Data { get; set; }
+        public string? ErrorMessage { get; set; }
+        public string? StatusCode { get; set; } // Map từ RpcException.StatusCode
+
+        public static GrpcResponse<T> Success(T data) => new() { IsSuccess = true, Data = data };
+
+        public static GrpcResponse<T> Failure(string message, string code = "Unknown")
+            => new() { IsSuccess = false, ErrorMessage = message, StatusCode = code };
+    }
+}
---
+++ Helper/GrpcClient/IGrpcClient.cs
@@ -0,0 +1,23 @@
+﻿using GrpcService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.GrpcClient
+{
+    public interface IGrpcClient
+    {
+        // User Service
+        Task<GrpcResponse<UserReply>> GetUserByIdAsync(string userId);
+
+        // Chat Service
+        Task<GrpcResponse<ConversationReply>> GetConversationByIdAsync(string conversationId);
+        Task<GrpcResponse<MessageReply>> GetMessageByIdAsync(string messageId);
+
+        // Notification Service
+        Task<GrpcResponse<NotificationMessageGrpcResponse>> NotifyNewMessageAsync(string conversationId, string messageId);
+        Task<GrpcResponse<NotificationMessageGrpcResponse>> NotifyUserActionAsync(string conversationId, string receiverId, string type, string dataJson);
+    }
+}
